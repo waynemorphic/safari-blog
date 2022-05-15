9ecb23c2255977dc850894a67b537928b249b754
waynemorphic <wayneklyde@gmail.com>
2022-05-15 23:02:39 +0300

    application runntime on server through flask script extension

--- manage.py
+++ manage.py
@@ -1,7 +1,17 @@
 from venv import create
-from application import create_app
+from application import create_app, db
+from flask_script import Manager,Server
+from application.models import User, Post, Comment
 
 app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User, Post = Post, Comment = Comment )
+
+
 if __name__ == '__main__':
-    app.run()
+    manager.run()
