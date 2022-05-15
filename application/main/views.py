50ef7cb0635f32dac3870350ec8e31d386b7f44f
waynemorphic <wayneklyde@gmail.com>
2022-05-15 23:04:19 +0300

    functions for various app functions

--- application/main/views.py
+++ application/main/views.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, url_for, redirect
+from urllib import request
+from flask import Flask, render_template, url_for, redirect, flash, abort
+from ..models import User, Post, Comment
+from application.auth.forms import RegistrationForm, LoginForm, PostForm, CommentForm, UpdateProfile
 from . import main
+from application import db, photos
 from flask_login import login_required, login_user, logout_user, current_user
+
 
 # index route
 @main.route('/')
@@ -13,40 +18,98 @@ def index():
     
     return render_template('index.html', title = title)
 
+# @main.route('/posts')
+# def posts():
+#     '''
+#     function defines all the posts in the application
+#     '''
+#     title = 'PostAPitch'
+#     return render_template()
+
 # log in
-@main.route('/login')
+@main.route('/login', methods=['GET', 'POST'])
 def login():
     '''
     function to define user login case
     '''
     
     title = 'PostAPitch'
-    
-    return render_template('auth/login.html', title = title)
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user, form.remeber.data)
+            return redirect(url_for('main.index'))
+        flash('Invalid Username or Password', 'danger')
+        
+    return render_template('auth/login.html', title = title, form = form)
 
 # registration
-@main.route('/register')
+@main.route('/register', methods=['GET', 'POST'])
 def registration():
     '''
     function to define registration case
     '''
     
     title = 'PostAPitch'
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration Successful', 'message')
+        return redirect(url_for('main.index'))
     
-    return render_template('auth/registration.html', title = title)
+    return render_template('auth/registration.html', title = title, form = form)
 
 @main.route('/logout')
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("main.login"))
 
+
 # user profile
-@main.route('/')
-def profile():
+@main.route('/user/<uname>')
+def profile(uname):
     '''
     function to define user profile
     '''
     
-    title = 'PostAPitch'
+    title = current_user.username
+    user = User.query.filter_by(username = uname).first()
     
-    return render_template('user/profile.html', title = title)
+    if user is None:
+        abort (404)
+    return render_template('user/profile.html', title = title, user = user)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form = form)
+
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname=uname))
